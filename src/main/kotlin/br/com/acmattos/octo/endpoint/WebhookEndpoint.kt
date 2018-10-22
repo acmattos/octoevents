45f621b018a6a1c393fa3308794e3742c844c1bc
André Mattos <acmattos@gmail.com>
2018-10-21 22:31:00 -0300

    - First commit

---
+++ src/main/kotlin/br/com/acmattos/octo/endpoint/WebhookEndpoint.kt
@@ -0,0 +1,57 @@
+package br.com.acmattos.octo.endpoint
+
+import br.com.acmattos.octo.event.Event
+import br.com.acmattos.octo.event.EventDeserializer
+import br.com.acmattos.octo.event.EventService
+import br.com.acmattos.octo.kodein
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import io.javalin.Context
+import org.kodein.di.generic.instance
+
+/**
+ * Deals with GitHub's event data processed by this application.
+ * @author acmattos
+ */
+class WebhookEndpoint() {
+   private val service: EventService by kodein.instance()
+
+   /**
+    * Process HTTP POST requests for http://host:port/webhooks
+    * Processes GitHub's event data sent to this resource.
+    *
+    * @param ctx Provides access to functions for handling the request and
+    *            response.
+    */
+   fun post(ctx: Context) {
+      var status: Int
+      try {
+         var payload: String = ctx.body()
+         val event = objectMapper().readValue(payload, Event::class.java)
+         if (event == null) {
+            status = 400
+            println("Event could not be persisted on DB: ${payload}")
+         } else {
+            service.save(event)
+            status = 201
+            println("Event persisted on DB: ${event}")
+         }
+      } catch (e: Exception){
+         e.printStackTrace()
+         status = 500
+      }
+      ctx.status(status)
+   }
+
+   /**
+    * Builds and configures ObjectMapper accordingly.
+    * @return ObjectMapper.
+    */
+   private fun objectMapper(): ObjectMapper {
+      val module = SimpleModule()
+      module.addDeserializer(Event::class.java, EventDeserializer(Event::class.java))
+      val mapper = ObjectMapper()
+      mapper.registerModule(module)
+      return mapper
+   }
+}
